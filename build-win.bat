5abc813a80a09d6432aeb99773e0781bd22e72b3
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2025-11-11 18:10:54 -0500

    Update github-build-release for compiler and stdlib

--- build-win.bat
+++ build-win.bat
@@ -1,59 +1,28 @@
 @echo on
 setlocal enabledelayedexpansion
 
+set triplet=%1
 set GENERATOR=Ninja
-
-set CONFIGS=Release Debug
+for /F %%p in ('powershell -Command "$((Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors)"') do set CORES=%%p
 
 if exist "update.bat" (
     call update.bat
 )
 
-set VS_VCVARSALL="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
-
-for /f %%p in ('wmic cpu get NumberOfLogicalProcessors ^| findstr /r /v "^$"') do set CORES=%%p
-
-REM --------------------------------
-REM Compile platforms
-REM --------------------------------
-call :compilePlatform x64         x64-windows
-call :compilePlatform amd64_arm64 arm64-windows
-
-echo Build complete!
-endlocal
-exit /b
-
-
-REM ==============================================
-REM Function: compilePlatform vcvars_arg triplet
-REM ==============================================
-:compilePlatform
-setlocal
-set build_mode=%1
-set triplet=%2
-
-echo ================================
-echo Building for %triplet% (%build_mode%)
-echo ================================
-
-call %VS_VCVARSALL% %build_mode%
-
 set BUILD_DIR=build\%triplet%
-if not exist "!BUILD_DIR!" mkdir "!BUILD_DIR!"
 set OUT_DIR=..\..\out
-if not exist "!OUT_DIR!" mkdir "!OUT_DIR!"
-for %%c in (%CONFIGS%) do (
-    echo Building %triplet%, config %%c in !BUILD_DIR!
+if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
+if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
 
-    cmake -S "." -B "!BUILD_DIR!" -G "%GENERATOR%" ^
-        -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" ^
-        -DVCPKG_TARGET_TRIPLET=%triplet%-static ^
-        -DCMAKE_BUILD_TYPE=%%c ^
-        -DAG_OUT_DIR=!OUT_DIR! ^
-        -DAG_TRIPLE=%triplet%
+echo Building %triplet% in %BUILD_DIR%
 
-    cmake --build "!BUILD_DIR!" --parallel !CORES!
-)
+cmake -S "." -B "%BUILD_DIR%" -G "%GENERATOR%" ^
+    -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" ^
+    -DVCPKG_TARGET_TRIPLET=%triplet%-static ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DAG_OUT_DIR=%OUT_DIR% ^
+    -DAG_TRIPLE=%triplet%
+
+cmake --build "%BUILD_DIR%" --parallel %CORES%
 
 endlocal
-exit /b
